432524816c20791b45e87379ea794ac73869fd79
falcaozane <112317470+falcaozane@users.noreply.github.com>
2023-05-02 22:02:26 +0530

    updated

--- Q 6/Q6.sql
+++ Q 6/Q6.sql
@@ -1,81 +1,88 @@
-CREATE TABLE sales_order_details (
- order_no VARCHAR(6),
- product_no VARCHAR(6),
- qty_ordered NUMERIC(8),
- qty_disp NUMERIC(8),
- product_rate NUMERIC(10,2),
- CONSTRAINT order_no_unique UNIQUE (order_no)
+Create Table Salesss( order_no
+varchar(6) PRIMARY KEY,
+product_no varchar(6),
+qty_ordered numeric(8), qty_disp
+numeric(8),
+product_rate numeric(10,2)
 );
-CREATE TABLE customer (
- cid INT,
- cname VARCHAR(50),
- address VARCHAR(100),
- pno VARCHAR(15),
- PRIMARY KEY (cid)
+select * from Salesss;
+insert into Salesss
+values('Or001','P001',12,10,100);
+insert into Salesss
+values('Or002','P002',22,11,200);
+insert into Salesss
+values('Or003','P003',32,12,300);
+insert into Salesss
+values('Or004','P004',42,13,400);
+insert into Salesss
+values('Or005','P005',52,14,500);
+Create Table customers( cid
+numeric(8) PRIMARY KEY, 
+cname varchar(8), address
+varchar(8),
+pno numeric(8))
+select * from customers;
+insert into customers
+values(1,'Namrata','Andheri',123);
+insert into customers
+values(2,'Neha','Dresden',345);
+insert into customers
+values(3,'Pearl','Parle',456); insert
+into customers
+values(4,'Risa','Vasai',567); insert
+into customers
+values(5,'Nimish','Virar',678);
+CREATE TABLE customer_orders
+(
+ cid INTEGER REFERENCES customers(cid),
+ order_no VARCHAR(6) REFERENCES Salesss(order_no)
 );
-CREATE TABLE cust_order (
- cid INT,
- order_no VARCHAR(6),
- FOREIGN KEY (cid) REFERENCES customer(cid),
- FOREIGN KEY (order_no) REFERENCES sales_order_details(order_no)
-);
-drop table sales_order_details;
-drop table customer;
-drop table cust_order;
-INSERT INTO sales_order_details VALUES
-('ORD001', 'P001', 10, 5, 100.00),
-('ORD002', 'P002', 5, 5, 50.00),
-('ORD003', 'P003', 20, 10, 200.00),
-('ORD004', 'P001', 15, 10, 100.00),
-('ORD005', 'P004', 8, 8, 80.00),
-('ORD006', 'P005', 12, 10, 120.00);
-INSERT INTO customer VALUES
-(101, 'John Doe', '123 Main St', '555-1234'),
-(102, 'Jane Smith', '456 Oak Ave', '555-5678'),
-(103, 'Bob Johnson', '789 Elm St', '555-9012'),
-(104, 'Mary Lee', '101 Pine St', '555-3456'),
-(105, 'Tom Jones', '234 Maple Ave', '555-7890');
-INSERT INTO cust_order VALUES
-(101, 'ORD001'),
-(102, 'ORD002'),
-(103, 'ORD003'),
-(101, 'ORD004'),
-(104, 'ORD005'),
-(105, 'ORD006');
-select*from sales_order_details;
-select*from customer;
-select*from cust_order;
---q3
-SELECT Product_no, SUM(Qty_ordered) AS total_qty_sold
-FROM sales_order_details
-GROUP BY Product_no;
---q4
-SELECT Product_no, SUM(Qty_disp * Product_rate) AS total_value_sold
-FROM sales_order_details
-GROUP BY Product_no;
---q5
-SELECT c.cname, AVG(s.Qty_ordered) AS avg_qty_sold
-FROM customer c
-JOIN cust_order co ON c.cid = co.cid
-JOIN sales_order_details s ON co.order_no = s.order_no
+select * from customer_orders;
+insert into customer_orders
+values(1,'Or001'); insert
+into customer_orders
+values(2,'Or002'); insert
+into customer_orders
+values(3,'Or003'); insert
+into customer_orders
+values(4,'Or004'); insert
+into customer_orders
+values(5,'Or005');
+select order_no,product_no,qty_disp from Salesss;
+select qty_disp*product_rate as total_value from Salesss;
+SELECT c.cid,
+c.cname,
+ AVG(s.qty_disp) AS avg_qty_sold
+FROM customers c
+JOIN customer_orders co ON c.cid = co.cid
+JOIN Salesss s ON co.order_no = s.order_no
 WHERE co.order_no IN (
  SELECT order_no
- FROM sales_order_details
- GROUP BY order_no
- HAVING SUM(Qty_disp * Product_rate) <= 15000
+ FROM Salesss
+ GROUP BY order_no 
+ HAVING SUM(product_rate * qty_disp) <= 15000
 )
-GROUP BY c.cname;
---q6
-SELECT SUM(Qty_disp * Product_rate) AS total_billed
-FROM sales_order_details
-WHERE order_no LIKE 'JAN%';
---q7
-SELECT DISTINCT c.cname
-FROM customer c
-JOIN cust_order co ON c.cid = co.cid
-JOIN sales_order_details s ON co.order_no = s.order_no
-WHERE s.Qty_ordered > 10;
---q8
-SELECT c.cname, s.Product_no, MAX(s.Qty_ordered) AS max_qty_ordered
-FROM customer c
-JOIN
+GROUP BY c.cid;
+SELECT c.cname
+FROM customers c
+JOIN customer_orders co ON c.cid = co.cid
+JOIN Salesss s ON co.order_no = s.order_no
+WHERE s.qty_ordered > 10;
+WITH max_qty AS (
+ SELECT product_no,
+ MAX(qty_disp) AS max_qty
+ FROM sales_order_details
+ GROUP BY product_no
+)
+WITH max_qty AS (
+ SELECT product_no,
+ MAX(qty_disp) AS max_qty
+ FROM Salesss
+ GROUP BY product_no
+)
+SELECT c.cname,
+ s.product_no
+FROM customers c
+JOIN customer_orders co ON c.cid = co.cid
+JOIN Salesss s ON co.order_no = s.order_no
+JOIN max_qty mq ON s.product_no = mq.product_no AND s.qty_disp = mq.max_qty; 
