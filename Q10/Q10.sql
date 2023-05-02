46d8be6f038c70db04b90fe5abaa76590c62d307
BastienA03 <114428030+BastienA03@users.noreply.github.com>
2023-05-02 16:05:23 +0530

    Update Q10.sql

---
+++ Q10/Q10.sql
@@ -0,0 +1,37 @@
+$$
+declare
+fact int;
+begin
+if n=0 then
+return 1;
+else
+fact:=n*fact(n-1);
+return fact;
+raise notice 'factorial of % is %',n,fact;
+end if;
+end;
+$$
+language plpgsql;
+select fact(5);
+
+drop table emp;
+create table emp(id int, name text, salary real);
+insert into emp values(1,'jenny',1000);
+insert into emp values(2,'janvi',2000);
+insert into emp values(3,'anuf',3000);
+select * from emp;
+create or replace function avg_salary() returns numeric(10,2) as
+$$
+declare
+total numeric(10,2):=0;
+count int:=0;
+avg numeric(10,2);
+begin
+select sum(salary), count(*) into total, count from emp;
+avg := total/count;
+RAISE NOTICE 'Average of salary is %', avg;
+RETURN avg;
+end;
+$$
+language plpgsql;
+select avg_salary();
